6fa82a6cf3bdebe6306e0216ebf3bce8e3bc9e1d
LuiSaad <luisa.lima2904@gmail.com>
2023-05-04 20:07:48 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/learning/login_layout.java
@@ -0,0 +1,13 @@
+package br.com.etecia.learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/learning/splash_layout.java
@@ -0,0 +1,14 @@
+package br.com.etecia.learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splash_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_splash);
+    }
+}
