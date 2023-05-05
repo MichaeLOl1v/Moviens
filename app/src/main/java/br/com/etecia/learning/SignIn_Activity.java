f19b23993dca40442deae176e8e92d594de7eb75
LuiSaad <luisa.lima2904@gmail.com>
2023-05-04 22:12:57 -0300

    1234567 1

---
+++ app/src/main/java/br/com/etecia/learning/SignIn_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignIn_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/learning/loginbandio_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginbandio_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginbandio_layout);
+    }
+}
